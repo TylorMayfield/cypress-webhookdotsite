274f9eeaa2e1705c4c2f9b1eb2343d038dcafda8
Tylor Mayfield <tmayfield@dealerpolicy.com>
2022-01-06 15:57:46 -0500

    add ts definitions

--- src/webHookSiteCommands.d.ts
+++ src/webHookSiteCommands.d.ts
@@ -1,1 +1,79 @@
 import Request from "./requestBuilder";
+
+/// <reference types="cypress" />
+
+/**
+ * Session token used to retrieve requests
+ */
+export interface token {
+  /**
+   * Token Value UUID
+   */
+  token?: string;
+}
+
+/**
+ * Paid token used to retrieve
+ */
+export interface apikey {
+  /**
+   * Api Key for paid accounts
+   */
+  apikey?: string;
+}
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Set API key if available
+       */
+      webHookDotSiteSetApiKey(
+        /**
+         * Set API key if available
+         */
+        apiKey: string
+      ): Cypress.Chainable<string>;
+
+      /**
+       * Retrieve session token
+       */
+      webHookDotSiteGetToken(
+        /**
+         * Retrieve session token
+         */
+        token: string
+      ): Cypress.Chainable<token>;
+
+      /**
+       * Return usable email address
+       */
+      webHookDotSiteEmailAddress(
+        /**
+         * Return usable email address
+         */
+        token: string
+      ): Cypress.Chainable<token>;
+
+      /**
+       * Return usable webhook uri
+       */
+      webHookDotSiteWebHookURI(
+        /**
+         * Return usable webhook uri
+         */
+        token: string
+      ): Cypress.Chainable<token>;
+
+      /**
+       * Return an webhook responses
+       */
+      webHookDotSiteGetResponses(
+        /**
+         * Return an webhook responses
+         */
+        token: string
+      ): Cypress.Chainable<token>;
+    }
+  }
+}
